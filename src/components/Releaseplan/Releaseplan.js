820e7998a715f4c8104659da3ab9a4f2e3e3de25
Victor Bergström <victor_bergstrom@hotmail.com>
2017-02-08 13:29:39 +0100

    solved error in releaseplan.js

--- src/components/Releaseplan/Releaseplan.js
+++ src/components/Releaseplan/Releaseplan.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReleaseplanTable from './ReleaseplanTable/ReleaseplanTable.js';
 
 export default class ReleasePlan extends React.Component {
 
     render(){
 	return (
 	  	<div className='releaseplan'>
-	  		<ReleaseplanTable />
 	  	</div>
 	  );
     }
