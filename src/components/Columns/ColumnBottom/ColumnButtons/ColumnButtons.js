2b11b9b7f28032ad920dd700220e74053e3778e7
AgnetaA <agnetaaxelsson@hotmail.se>
2017-02-13 14:26:05 +0100

    button

--- src/components/Columns/ColumnBottom/ColumnButtons/ColumnButtons.js
+++ src/components/Columns/ColumnBottom/ColumnButtons/ColumnButtons.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './ColumnButtons.css';
 
 export default class ColumnButtons extends React.Component {
 	render() {
 		return (
 			<div className='column-buttons'>
 				
-				
+				<button>Go!</button>
 				
 			</div>
 		);
