727987dd2de4d5e845c726b103186836cb32ac5e
Max <max.trewhitt@student.kyh.se>
2017-02-15 13:48:53 +0100

    dynamic card stacks, random positioned cards.

--- src/components/Cards/Defects/Defects.js
+++ src/components/Cards/Defects/Defects.js
@@ -3,14 +3,20 @@ import React from 'react';
 
 export default class Defects extends React.Component {
 	componentDidMount() {
+
+		// Insert points
 		insertLetters(this.props.analytics, this.refs.analytics, 'A');
 		insertLetters(this.props.development, this.refs.development, 'D');
 		insertLetters(this.props.test, this.refs.test, 'T');
+
+		// Randomize card position
+		var card = this.refs.card;
+		randomCardPos(card);
 	}
 
 	render() {
 		return (
-			<div className='defects-card'>
+			<div ref='card' className='defects-card'>
 				<p className='header'>D{this.props.number}</p>
 				<div className='points'>
 					<div ref='analytics'></div>
@@ -20,6 +26,21 @@ export default class Defects extends React.Component {
 			</div>
 		);
 	}
+}
+
+function randomCardPos(card) {
+
+	var randomNumber = Math.random();
+	var top = (Math.random() * 10).toString() + 'px';
+
+	if (randomNumber > 0.5) {
+		var left = (randomNumber * 10).toString() + 'px';
+	} else {
+		var left = (randomNumber * -10).toString() + 'px';
+	}
+
+	card.style.top = top;
+	card.style.left = left;
 }
 
 function insertLetters(amount, ref, letter) {
--- src/components/Cards/UserStory/User_Story.js
+++ src/components/Cards/UserStory/User_Story.js
@@ -4,14 +4,20 @@ import './User_Story.css';
 export default class UserStory extends React.Component {
 
 	componentDidMount() {
+
+		// Insert points
 		insertLetters(this.props.analytics, this.refs.analytics, 'A');
 		insertLetters(this.props.development, this.refs.development, 'D');
 		insertLetters(this.props.test, this.refs.test, 'T');
+
+		// Randomize card position
+		var card = this.refs.card;
+		randomCardPos(card);
 	}
 
     render(){
 	return (
-	  	<div className='user-story'>
+	  	<div ref='card' className='user-story'>
 	  		<p className='header'>US{this.props.number}</p><p className='value'>${this.props.value}</p>
 	  		<div className='points'>
 	  			<div ref='analytics'></div>
@@ -21,6 +27,21 @@ export default class UserStory extends React.Component {
 	  	</div>
 	  );
     }
+}
+
+function randomCardPos(card) {
+
+	var randomNumber = Math.random();
+	var top = (Math.random() * 10).toString() + 'px';
+
+	if (randomNumber > 0.5) {
+		var left = (randomNumber * 10).toString() + 'px';
+	} else {
+		var left = (randomNumber * -10).toString() + 'px';
+	}
+
+	card.style.top = top;
+	card.style.left = left;
 }
 
 function insertLetters(amount, ref, letter) {
