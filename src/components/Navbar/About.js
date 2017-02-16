2347c2a3394eb0360bac5a73052dbbd4aad2a108
Henge <perhenric@gmail.com>
2017-02-16 15:53:14 +0100

    working with show hide from navbar

---
+++ src/components/Navbar/About.js
@@ -0,0 +1,13 @@
+import './Navbar.css';
+import React from 'react';
+
+export default class About extends React.Component {
+
+    render(){
+		return (
+		  	<div>
+		  		<p>en massa blaj About</p>
+		  	</div>
+		);
+    }
+}
