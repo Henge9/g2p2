af7fd4d19d3907c5536c6753af69f5908e579652
Victor Bergström <victor_bergstrom@hotmail.com>
2017-02-21 15:11:39 +0100

    die1

--- src/components/Columns/ColumnBottom/DiceArea/Dice/Die1.js
+++ src/components/Columns/ColumnBottom/DiceArea/Dice/Die1.js
@@ -3,10 +3,20 @@ import './Dice.css';
 
 
 export default class Die1 extends React.Component {
+
 	render() {
+
+		if(!true){
+			var stylee = {backgroundImage: 'url(' + './DicePics/Six.png' + ')'};
+			// 							   'url(' + imgUrl 			   + ')'
+			// var stylee = {background: 'red'};
+		}
+
 		return (
-			<div className='die1'>
+			<div className="die1" style={'backgroundImage: `url(${stylee})`'}>
 			</div>
 		);
 	}
 }
+
+// backgroundImage: `url(${Background})`
