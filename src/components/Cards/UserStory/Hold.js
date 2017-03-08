65693c6d5a431eb837adba5155fbb3473e605fb2
Max <max.trewhitt@student.kyh.se>
2017-03-08 15:22:13 +0100

    userstories now drawn from database

--- src/components/Cards/UserStory/Hold.js
+++ src/components/Cards/UserStory/Hold.js
@@ -5,35 +5,47 @@ import UserStory from './User_Story.js';
 
 var card;
 var cards = [];
-var jsx = [];
 
 export default class Hold extends React.Component {
 
-	generateCards() {
+	constructor() {
+		super();
+		this.state = {
+			jsx: [],
+		}
+		this.componentWillMount = this.componentWillMount.bind(this);
+	}
+
+	componentWillMount() {
 		
+		self = this;
+
 		axios.get('http://localhost:8080/g2p2/src/ajax/Card_Info.php').then(function(result) {
+
+			let jsx = [];
 
 			// Ajax code
 			cards = result.data;
 
 			// Create JSX elements
 			for (var i = 0; i < cards.length; i++) {
+
 				card = <UserStory number={cards[i].number} value={cards[i].value} analytics={cards[i].analytics} development={cards[i].development} test={cards[i].test}/>
+
+				jsx = self.state.jsx;
 				jsx.push(card);
+
+				self.setState({
+					jsx: jsx
+				});
 			}
-
-			console.log(jsx);
-
-			// Return array of JSX elements
-			return jsx;
 		});
 	}
 
     render() {
-
     	return (
     	<div className='hold'>
-    		{console.log(this.generateCards())}
+    		{this.state.jsx}
     	</div>
     	);
     }
