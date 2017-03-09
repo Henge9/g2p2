b73f8628e9f4d5e01ee604c5bab989718d6ec0a3
AgnetaA <agnetaaxelsson@hotmail.se>
2017-03-09 16:39:32 +0100

    changed url for dice images

--- src/components/Columns/ColumnBottom/DiceArea/Dice/Die1.js
+++ src/components/Columns/ColumnBottom/DiceArea/Dice/Die1.js
@@ -51,22 +51,22 @@ export default class Die1 extends React.Component {
 		var numb = this.props.rollDice1[`numberI`];
 		
 		if(numb === 1) {
-		 	var d1 = { backgroundImage: "url('http://agnetaaxelsson.se/dice/One2.png')" };
+		 	var d1 = { backgroundImage: "url('http://agileboardgame.com/diceimg/One2.png')" };
 		}
 		else if(numb === 2) {
-		 	d1 = { backgroundImage: "url('http://agnetaaxelsson.se/dice/Two2.png')" }; 	
+		 	d1 = { backgroundImage: "url('http://agileboardgame.com/diceimg/Two2.png')" }; 	
 		}
 		else if(numb === 3) {	
-		 	d1 = { backgroundImage: "url('http://agnetaaxelsson.se/dice/Three2.png')" };
+		 	d1 = { backgroundImage: "url('http://agileboardgame.com/diceimg/Three2.png')" };
 		} 	
 		else if(numb === 4) {
-			d1 = { backgroundImage: "url('http://agnetaaxelsson.se/dice/Four2.png')" };
+			d1 = { backgroundImage: "url('http://agileboardgame.com/diceimg/Four2.png')" };
 		}	
 		else if(numb === 5) {	
-			d1 = { backgroundImage: "url('http://agnetaaxelsson.se/dice/Five2.png')" };	
+			d1 = { backgroundImage: "url('http://agileboardgame.com/diceimg/Five2.png')" };	
 		}
 		else if(numb === 6) {		
-			d1 = { backgroundImage: "url('http://agnetaaxelsson.se/dice/Six2.png')" };
+			d1 = { backgroundImage: "url('http://agileboardgame.com/diceimg/Six2.png')" };
 		}
 
 
--- src/components/Columns/ColumnBottom/DiceArea/Dice/Die5.js
+++ src/components/Columns/ColumnBottom/DiceArea/Dice/Die5.js
@@ -47,22 +47,22 @@ export default class Die5 extends React.Component {
 		var numb = this.props.rollDice5[`numberV`];
 
 		if(numb === 1) {
-			var d5 = { backgroundImage: "url('http://agnetaaxelsson.se/dice/One.png')" };
+			var d5 = { backgroundImage: "url('http://agileboardgame.com/diceimg/One.png')" };
 		}
 		else if(numb === 2) {
-		 	d5 = { backgroundImage: "url('http://agnetaaxelsson.se/dice/Two.png')" }; 	
+		 	d5 = { backgroundImage: "url('http://agileboardgame.com/diceimg/Two.png')" };	 	
 		}
 		else if(numb === 3) {
-		 	d5 = { backgroundImage: "url('http://agnetaaxelsson.se/dice/Three.png')" };
+		 	d5 = { backgroundImage: "url('http://agileboardgame.com/diceimg/Three.png')" }
 		} 	
 		else if(numb === 4) {
-			d5 = { backgroundImage: "url('http://agnetaaxelsson.se/dice/Four.png')" };
+			d5 = { backgroundImage: "url('http://agileboardgame.com/diceimg/Four.png')" };
 		}	
 		else if(numb === 5) {
-			d5 = { backgroundImage: "url('http://agnetaaxelsson.se/dice/Five.png')" };	
+			d5 = { backgroundImage: "url('http://agileboardgame.com/diceimg/Five.png')" };	
 		}
 		else if(numb === 6) {
-			d5 = { backgroundImage: "url('http://agnetaaxelsson.se/dice/Six.png')" };
+			d5 = { backgroundImage: "url('http://agileboardgame.com/diceimg/Six.png')" };
 		}
 
 	
