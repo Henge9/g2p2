776e2b9e4b1e795ccb39b2979ce2732aade85fce
Victor Bergström <victor_bergstrom@hotmail.com>
2017-03-23 20:36:59 +0100

    axios get highscore

--- src/components/Navbar/Highscore.js
+++ src/components/Navbar/Highscore.js
@@ -1,9 +1,22 @@
 import './Navbar.css';
 import React from 'react';
+import axios from 'axios';
 
 export default class Highscore extends React.Component {
 
+	// printHighscore(){
+	// 	axios.get('http://agileboardgame.com/rest/?/highscore')
+	// 	.then(function(result) {
+	// 		console.log(result.data)
+	// 	})
+	// }
+
     render(){
+    	axios.get('http://agileboardgame.com/rest/?/highscore')
+		.then(function(result) {
+			console.log(result.data)
+		})
+    	
 		return (
 		  	<div className="nav-component highscore">
 		  		<div className="nav-close" onClick={(e) => {this.props.navClose(e)}}>X</div>
--- src/components/Navbar/SaveGame.js
+++ src/components/Navbar/SaveGame.js
@@ -1,15 +1,50 @@
 import './Navbar.css';
 import React from 'react';
+import axios from 'axios';
 
 export default class Highscore extends React.Component {
+	constructor(){
+		super()
+		this.onChange = this.onChange.bind(this)
+		this.sendName = this.sendName.bind(this)
+		this.state = {
+			inputValue: '',
+		}
+	}
+
+	onChange(event){
+		this.setState({inputValue: event.target.value})
+	}
+
+	sendName(){
+		var inputValue = this.refs.teamNameInput.value
+		console.log(this.state.inputValue)
+
+		// axios.get('http://agileboardgame.com/rest/?/us_cards').then(function(result)
+
+		axios.post('http://agileboardgame.com/rest/?/highscore', {
+		    team: 'Fred',
+		    score: 'Flintstone'
+	    })
+	    .then(function (response) {
+		    console.log('response: ', response);
+	    })
+	    .catch(function (error) {
+	    	console.log('error: ', error);
+	  	});
+	}
+
+	sendPoints(){
+
+	}
 
     render(){
 		return (
 		  	<div className="nav-component highscore">
 		  		<div className="nav-close" onClick={(e) => {this.props.navClose(e)}}>X</div>
 		  		<label>Enter teamname:</label>
-		  		<input/>
-		  		<input type="submit"/>
+		  		<input ref='teamNameInput' onChange={this.onChange}/>
+		  		<input type="submit" onClick={this.sendName}/>
 		  		<label>Total points: ${this.props.earnedTotalSumState}</label>
 		  		<p></p>
 		  	</div>
